bc67ee3db06008ad932ad93c520911181b7c69a5
erenhncr <erenhncr@gmail.com>
2021-02-05 13:16:13 +0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+      arrowFunctions: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/destructuring-assignment': [0],
+    'import/no-named-as-default': 0,
+    'no-param-reassign': 0,
+    'jsx-a11y/label-has-associated-control': [
+      2,
+      {
+        labelComponents: ['CustomInputLabel'],
+        labelAttributes: ['label'],
+        controlComponents: ['CustomInput'],
+        assert: 'htmlFor',
+        depth: 3,
+      },
+    ],
+  },
+};
---
+++ src/InputWithStatus.js
@@ -0,0 +1,61 @@
+import React, { useMemo, useState } from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+function InputWithStatus({
+  id, name, type, val, placeholder, onChange,
+}) {
+  const [showPass, setShowPass] = useState(false);
+  const passwordType = useMemo(() => {
+    if (showPass) {
+      return 'text';
+    }
+    return 'password';
+  }, [showPass, setShowPass]);
+
+  return (
+    <div className="input-with-status">
+      <i
+        id={`icon-${id}`}
+        className={classnames({
+          bx: true,
+          'input-icon': true,
+          'bx-envelope': id === 'email',
+          'bx-key': id === 'password',
+        })}
+      />
+      <input
+        autoComplete="off"
+        id={id}
+        name={name}
+        type={type === 'password' ? passwordType : type}
+        onChange={onChange}
+        value={val}
+        placeholder={placeholder}
+      />
+      {id !== 'password' && <i className="bx bx-error-circle" />}
+      {id === 'password' && (
+        /* eslint-disable-next-line */
+        <i
+          className={classnames({
+            bx: true,
+            'bx-hide': !showPass,
+            'bx-show': showPass,
+          })}
+          onClick={() => setShowPass(!showPass)}
+        />
+      )}
+    </div>
+  );
+}
+
+InputWithStatus.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  val: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default InputWithStatus;
