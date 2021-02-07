0faf65ba266011c8120460d79c2ab834728de136
erenhncr <erenhncr@gmail.com>
2021-02-07 18:57:20 +0300

    flagItem name changed to flagIcon

--- src/FlagIcon.js
+++ src/FlagIcon.js
@@ -3,32 +3,7 @@ import PropTypes from 'prop-types';
 import {
   Spinner,
 } from 'reactstrap';
-import EN from './assets/flags/en.svg';
-import DE from './assets/flags/de.svg';
-import FR from './assets/flags/fr.svg';
-import TR from './assets/flags/tr.svg';
-
-const getFlagUrl = (lang) => {
-  let flag = EN;
-  switch (lang) {
-    case 'EN':
-      flag = EN;
-      break;
-    case 'DE':
-      flag = DE;
-      break;
-    case 'FR':
-      flag = FR;
-      break;
-    case 'TR':
-      flag = TR;
-      break;
-    default:
-      flag = EN;
-      break;
-  }
-  return flag;
-};
+import getFlagUrl from './assets/flags';
 
 function FlagItem({ language }) {
   return (
